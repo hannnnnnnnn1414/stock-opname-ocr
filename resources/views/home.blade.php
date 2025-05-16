9dfcd7eb34929f6fbada19ef32dc1eb8baaaf9bf
hannnnnnnnn1414 <haniffathurrahmanrustanto@gmail.com>
2025-05-16 13:32:49 +0700

    Centered footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -171,19 +171,7 @@
       </div>
 
       <footer class="footer pt-3">
-        <div class="container-fluid">
-            <div class="row align-items-center justify-content-lg-between">
-                <div class="col-lg-6 mb-lg-0 mb-4">
-                    <div class="copyright text-center text-sm text-muted text-lg-start">
-                        COPYRIGHT @ PT KAYABA INDONESIA
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                    </ul>
-                </div>
-            </div>
-        </div>
+        @include('components.footer')
       </footer>
     </div>
   </main>
